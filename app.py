81405518b88ad2c2a193afa156c603bc7e51364c
EmersonTo <48694353+EmersonTo@users.noreply.github.com>
2023-06-25 18:32:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import dash
 import dash_bootstrap_components as dbc
-
+from dash_bootstrap_templates import load_figure_template
 # SLATE
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SLATE])
